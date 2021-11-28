1919f88e73416b1088e51aa35f46e43117d871a9
Eugenio Carvalho <carvalhoalberto03@gmail.com>
2021-11-28 14:49:52 -0300

    project created

---
+++ src/test/java/com/workshopspring/workshopspringmongo/WorkshopSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopspring.workshopspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
