b512a6259f317c49f2f3541f8c3c65b2e373a90b
Eugenio Carvalho <carvalhoalberto03@gmail.com>
2021-11-28 21:52:18 -0300

    Entity user e REST

--- src/main/java/com/workshopspring/workshopspringmongo/Application.java
+++ src/main/java/com/workshopspring/workshopspringmongo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkshopSpringMongoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopSpringMongoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
